d946a1cd2446330c9db93554540f5358045a9ffc
jmpcordova19 <129245114+jmpcordova19@users.noreply.github.com>
2023-06-28 22:30:28 -0700

    initial

---
+++ mycode.js
@@ -0,0 +1,30 @@
+let name = 'Amelia';
+let town_name = 'Willowbrook';
+let abandoned_building = 'Forsaken Manor';
+let shadow_name = 'Morbidos Vileshade';
+
+
+let story2 = `In the eerie town of ${town_name}, shrouded in perpetual mist, there resided a mysterious figure known only as ${shadow_name}. This enigmatic being was said to be cloaked in darkness, with piercing ominous blood red eyes that seemed to penetrate one's soul.
+
+${shadow_name} was a legend whispered among the terrified townsfolk, who locked their doors at night and avoided the shadows that seemed to dance with malice. No one dared to venture near the desolate ${abandoned_building} that served as ${shadow_name}'s dwelling, for fear of the unknown horrors that lurked within.
+
+But there was one brave soul, a young woman named ${name}, who defied the warnings and set out to uncover the truth about ${shadow_name}. Her silver streak hair flowed in the wind as she made her way towards the decrepit building, determined to face the darkness that gripped the town.
+
+As ${name} stepped into the creaking entrance of the ${abandoned_building}, a bone-chilling silence engulfed her. Each step echoed ominously, as if the very walls were whispering haunting secrets. The air grew colder, and an unsettling presence weighed upon her shoulders.
+
+In the dim light, ${name} spotted a flickering candle, its feeble flame struggling against the encroaching darkness. She cautiously approached it, the soft glow revealing an ancient book with tattered pages. Its leather cover bore an eerie inscription that seemed to pulse with an otherworldly energy.
+
+Driven by a mix of fear and curiosity, ${name} dared to open the book. As she turned the pages, wicked tales of forgotten horrors emerged, each more chilling than the last. The words seemed to twist and writhe, as if trying to escape their ominous confines.
+
+Suddenly, a shadowy figure materialized before ${name}, emanating a malevolence that sent shivers down her spine. It was ${shadow_name}, now corporeal and hungry for souls. His eyes locked onto hers, freezing her in terror as he revealed his ghastly plan to consume the souls of the living.
+
+With every ounce of courage she possessed, ${name} fought against the encroaching darkness. She recited ancient incantations, banishing ${shadow_name} back into the abyss from which he came. The very foundations of the ${abandoned_building} shook with his defeat, as if the town itself rejoiced at the vanquishing of its nightmare.
+
+As ${name} emerged from the ${abandoned_building}, she was hailed as a hero by the townsfolk. They celebrated her bravery and thanked her for lifting the veil of fear that had plagued their lives for so long. ${town_name} began to heal, slowly returning to its former glory.
+
+But as night fell, a chilling wind whispered through the streets, reminding everyone that darkness can never truly be banished. It lingers in the shadows, waiting for its chance to return. And the legend of ${shadow_name} would forever be etched in the haunted history of ${town_name}, a reminder of the thin line that separates the living from the realm of nightmares.`;
+
+
+let mypar = document.querySelector('p');
+
+mypar.innerText = story2;
